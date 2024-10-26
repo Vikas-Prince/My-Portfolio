34e9dbc2f34616b49de129e561b9ecfc65333e86
Vikas-Prince <vikasprince30809@gmail.com>
2024-10-26 12:59:28 +0530

    Added new Project category

--- Script.js
+++ Script.js
@@ -83,6 +83,34 @@ var projects = [
   // },
 ];
 
+var dev_projects = [
+  {
+    image: "Images/freshcart.png",
+    title: "Complete End to End Pipeline",
+    repoLink: "https://github.com/Vikas-Prince/End-to-End-CI-CD-Pipeline.git",
+  },
+  {
+    image: "Images/facebook.png",
+    title: "Microservices Deployment",
+    repoLink: "https://github.com/Vikas-Prince/MicroservicesAppDeployment.git",
+  },
+  {
+    image: "Images/k8s_architect.png",
+    title: "K8s Full Stack Deployment",
+    repoLink: "https://github.com/Vikas-Prince/K8s-FullStack-Deployment.git",
+  },
+  {
+    image: "Images/netflix.png",
+    title: "Reddit App on AWS EKS",
+    repoLink: "https://github.com/Vikas-Prince/Reddit-App-on-AWS-EKS.git",
+  },
+  {
+    image: "Images/freshcart.png",
+    title: "JavaApp Deployment",
+    repoLink: "https://github.com/Vikas-Prince/JavaApplicationDeployment.git",
+  },
+];
+
 var typed = new Typed(".role", {
   strings: [
     "Full Stack Developer. . .",
@@ -120,6 +148,62 @@ ScrollReveal().reveal(".slider", { origin: "bottom" });
 // ScrollReveal().reveal(".c-slider", { origin: "bottom" });
 
 $(document).ready(function () {
+  var $devprojectContainer = $("#DevOps_container");
+
+  // Loop through each project and append to the container
+  dev_projects.forEach(function (proj) {
+    var projectHTML = `
+                    <div class="slider-container card1">
+                        <div class="project-1">
+                            <img src="${proj.image}" alt=""><br>
+                            <h3>${proj.title}</h3>
+                            <div class="btn-section btn-center">
+                              <a href="${proj.repoLink}" target="_blank" class="git-btn" >Git Repo</a>
+                            </div>
+                        </div>
+                    </div>
+                `;
+    $devprojectContainer.append(projectHTML);
+  });
+
+  $devprojectContainer.slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    dots: true,
+    speed: 1000,
+    autoplaySpeed: 2000,
+    prevArrow:
+      '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill left-arrow-icon" viewBox="0 0 16 16"><path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/></svg>',
+    nextArrow:
+      '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-left-fill right-arrow-icon" viewBox="0 0 16 16"><path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/></svg>',
+
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+
+      {
+        breakpoint: 570,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+
   var $projectContainer = $("#projectContainer");
 
   // Loop through each project and append to the container
